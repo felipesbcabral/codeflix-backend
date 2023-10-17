d48d1333b58fa26a0c11e38ef1ed2b093c2dd23c
FelipeCabralz <felipesbcabral@gmail.com>
2023-10-17 00:20:52 -0300

    :sparkles: Creates project and creates tests using TDD for new entity

---
+++ FC.Codeflix.Catalog.Domain/Entity/Category.cs
@@ -0,0 +1,57 @@
+﻿using FC.Codeflix.Catalog.Domain.Exceptions;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Headers;
+
+namespace FC.Codeflix.Catalog.Domain.Entity;
+public class Category
+{
+    public Guid Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public string Description { get; private set; }
+
+    public bool IsActive { get; private set; }
+
+    public DateTime CreatedAt { get; private set; }
+
+    public Category(string name, string description, bool isActive = true)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Description = description;
+        IsActive = isActive;
+        CreatedAt = DateTime.Now;
+        Validate();
+    }
+
+    public void Activate()
+    {
+        IsActive = true;
+        Validate();
+    }
+
+    public void Deactivate()
+    {
+        IsActive = false;
+        Validate();
+    }
+
+    private void Validate() 
+    {
+        if (String.IsNullOrWhiteSpace(Name))
+            throw new EntityValidationException($"{nameof(Name)} should not be empty or null");
+        
+        if (Name.Length < 3)
+            throw new EntityValidationException($"{nameof(Name)} should be at least 3 characters long");
+
+        if (Name.Length > 255)
+            throw new EntityValidationException($"{nameof(Name)} should be less or equal to 255 characters long");
+
+        if (Description == null)
+            throw new EntityValidationException($"{nameof(Description)} should not be null");
+
+        if (Description.Length > 10000)
+            throw new EntityValidationException($"{nameof(Description)} should be less or equal to 10000 characters long");
+    }
+}
