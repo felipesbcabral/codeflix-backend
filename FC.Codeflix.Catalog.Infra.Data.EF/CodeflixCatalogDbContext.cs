1f0be3757003f06d8e29bd80d349a40627fb3f54
FelipeCabralz <felipesbcabral@gmail.com>
2023-10-28 22:01:38 -0300

    feat: creates-new-infra-project

---
+++ FC.Codeflix.Catalog.Infra.Data.EF/CodeflixCatalogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Infra.Data.EF.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FC.Codeflix.Catalog.Infra.Data.EF;
+public class CodeflixCatalogDbContext : DbContext
+{
+    public DbSet<Category> Categories => Set<Category>();
+
+    public CodeflixCatalogDbContext(DbContextOptions<CodeflixCatalogDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+    }
+}
---
+++ FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly CodeflixCatalogDbContext _context;
+    private DbSet<Category> _categories
+        => _context.Set<Category>();
+
+    public CategoryRepository(CodeflixCatalogDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Insert(Category aggregate, CancellationToken cancellationToken)
+        => await _categories.AddAsync(aggregate, cancellationToken);
+
+    public async Task<Category> Get(Guid id, CancellationToken cancellationToken)
+    {
+        var category = await _categories.FindAsync(
+                new object[] { id },
+                cancellationToken
+            );
+
+        NotFoundException.ThrowIfNull(category, $"Category '{id}' nof found");
+        return category!;
+    }
+
+    public Task Delete(Category aggregate, CancellationToken cancellationToken)
+        => throw new NotImplementedException();
+
+
+    public Task<SearchOutput<Category>> Search(SearchInput input, CancellationToken cancellationToken)
+        => throw new NotImplementedException();
+
+    public Task<Category> Update(Category aggregate, CancellationToken cancellationToken)
+        => throw new NotImplementedException();
+}
