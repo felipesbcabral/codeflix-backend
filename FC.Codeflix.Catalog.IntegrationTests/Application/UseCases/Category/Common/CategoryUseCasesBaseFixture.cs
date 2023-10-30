cf8774038adbeb7ad932a2ea31e7dfab596ca4b3
FelipeCabralz <felipesbcabral@gmail.com>
2023-10-30 19:50:12 -0300

    test: implementing integrated tests for use case get category

---
+++ FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/Common/CategoryUseCasesBaseFixture.cs
@@ -0,0 +1,40 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+public class CategoryUseCasesBaseFixture : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription =
+            Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10000)
+            categoryDescription =
+                categoryDescription[..10000];
+        return categoryDescription;
+    }
+
+    public static bool GetRandomBoolean()
+        => new Random().NextDouble() < 0.5;
+
+    public DomainEntity.Category GetExampleCategory()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+
+    public List<DomainEntity.Category> GetExampleCategoriesList(int length = 10)
+    => Enumerable.Range(1, length)
+        .Select(_ => GetExampleCategory()).ToList();
+}
