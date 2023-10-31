2685a3974d1635430fade2e24885a8a01d7046bf
FelipeCabralz <felipesbcabral@gmail.com>
2023-10-31 19:03:57 -0300

    feat: api setup configurations

---
+++ FC.Codeflix.Catalog.Api/Configurations/ControllersConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace FC.Codeflix.Catalog.Api.Configurations;
+
+public static class ControllersConfiguration
+{
+    public static IServiceCollection AddAndConfigureControllers(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddDocumentation();
+        return services;
+    }
+
+    private static IServiceCollection AddDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+
+    public static WebApplication UseDocumentation(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        return app;
+    }
+}
---
+++ FC.Codeflix.Catalog.Api/Configurations/UseCasesConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using MediatR;
+
+namespace FC.Codeflix.Catalog.Api.Configurations;
+
+public static class UseCasesConfiguration
+{
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        var allUseCases = typeof(CreateCategory);
+        services.AddMediatR(allUseCases);
+        services.AddRepositories();
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<
+            ICategoryRepository, CategoryRepository>();
+        services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
