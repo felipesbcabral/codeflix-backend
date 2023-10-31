c7e3aef8d1aa042a7dbba30199ff3e825b8fd615
FelipeCabralz <felipesbcabral@gmail.com>
2023-10-31 17:04:54 -0300

    test: setup first end to end test

---
+++ FC.Codeflix.Catalog.EndToEndTests/Api/Category/CreateCategory/CreateCategoryApiTest.cs
@@ -0,0 +1,46 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
+using FluentAssertions;
+using Xunit;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Category.CreateCategory;
+
+[CollectionDefinition(nameof(CreateCategoryApiTestFixture))]
+public class CreateCategoryApiTest
+{
+    private readonly CreateCategoryApiTestFixture _fixture;
+
+    public CreateCategoryApiTest(CreateCategoryApiTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(Create))]
+    [Trait("EndToEnd/Api", "Category - Endpoints")]
+    public async Task Create()
+    {
+        var input = _fixture.getExampleInput();
+
+        CategoryModelOutput output = await _fixture.
+            ApiClient.Post<CategoryModelOutput>(
+                "/categories",
+                input
+            );
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().BeSameDateAs(default);
+        DomainEntity.Category dbCategory = await _fixture.Persistence
+            .GetById(output.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be(input.IsActive);
+        dbCategory.Id.Should().NotBeEmpty();
+        dbCategory.CreatedAt.Should()
+            .NotBeSameDateAs(default);
+    }
+}
