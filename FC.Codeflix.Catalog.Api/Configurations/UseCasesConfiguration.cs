03a7f6f5b7d6aba62b0b2773fe9f3984e142f52f
FelipeCabralz <felipesbcabral@gmail.com>
2023-11-01 16:29:10 -0300

    feat: implementing categories controller and creating create category unit test

--- FC.Codeflix.Catalog.Api/Configurations/UseCasesConfiguration.cs
+++ FC.Codeflix.Catalog.Api/Configurations/UseCasesConfiguration.cs
@@ -3,26 +3,25 @@ using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 using FC.Codeflix.Catalog.Domain.Repository;
 using FC.Codeflix.Catalog.Infra.Data.EF;
 using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
-using MediatR;
 
 namespace FC.Codeflix.Catalog.Api.Configurations;
 
 public static class UseCasesConfiguration
 {
-    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    public static IServiceCollection AddUseCases(
+        this IServiceCollection services
+    )
     {
-        var allUseCases = typeof(CreateCategory);
-        services.AddMediatR(allUseCases);
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateCategory>());
         services.AddRepositories();
         return services;
     }
 
-    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    private static IServiceCollection AddRepositories(
+            this IServiceCollection services)
     {
-        services.AddTransient<
-            ICategoryRepository, CategoryRepository>();
+        services.AddTransient<ICategoryRepository, CategoryRepository>();
         services.AddTransient<IUnitOfWork, UnitOfWork>();
-
         return services;
     }
 }
