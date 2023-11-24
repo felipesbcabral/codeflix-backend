c84f0ced3f85ec3c779506538fb67c48ec1403ea
FelipeCabralz <felipesbcabral@gmail.com>
2023-11-24 16:49:05 -0300

    refactor:

--- src/FC.Codeflix.Catalog.Infra.Data.EF/Configurations/CategoryConfiguration.cs
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Configurations/CategoryConfiguration.cs
@@ -1,17 +1,16 @@
-﻿using FC.Codeflix.Catalog.Infra.Data.EF.Models;
+﻿using FC.Codeflix.Catalog.Domain.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace FC.Codeflix.Catalog.Infra.Data.EF.Configurations;
-internal class GenresCategoriesConfigurations : IEntityTypeConfiguration<GenresCategories>
+internal class GenresCategoriesConfigurations : IEntityTypeConfiguration<Category>
 {
-    public void Configure(EntityTypeBuilder<GenresCategories> builder)
+    public void Configure(EntityTypeBuilder<Category> builder)
     {
-        builder.HasKey(category => new
-        {
-            category.CategoryId,
-            category.GenreId
-        });
-
+        builder.HasKey(category => category.Id);
+        builder.Property(category => category.Name)
+            .HasMaxLength(255);
+        builder.Property(category => category.Description)
+            .HasMaxLength(10_000);
     }
 }
--- src/FC.Codeflix.Catalog.Infra.Data.EF/Configurations/GenresCategoriesConfiguration.cs
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Configurations/GenresCategoriesConfiguration.cs
@@ -1,16 +1,17 @@
-﻿using FC.Codeflix.Catalog.Domain.Entity;
+﻿using FC.Codeflix.Catalog.Infra.Data.EF.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace FC.Codeflix.Catalog.Infra.Data.EF.Configurations;
-internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+internal class CategoryConfiguration : IEntityTypeConfiguration<GenresCategories>
 {
-    public void Configure(EntityTypeBuilder<Category> builder)
+    public void Configure(EntityTypeBuilder<GenresCategories> builder)
     {
-        builder.HasKey(category => category.Id);
-        builder.Property(category => category.Name)
-            .HasMaxLength(255);
-        builder.Property(category => category.Description)
-            .HasMaxLength(10_000);
+        builder.HasKey(category => new
+        {
+            category.CategoryId,
+            category.GenreId
+        });
+
     }
 }
