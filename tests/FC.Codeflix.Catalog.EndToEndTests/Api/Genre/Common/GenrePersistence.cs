aa0ac20d2d60a345b5669bc0c9803d132d8a4847
FelipeCabralz <felipesbcabral@gmail.com>
2023-12-03 21:35:27 -0300

    feat: implementing genre detais endpoint

---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/Common/GenrePersistence.cs
@@ -0,0 +1,16 @@
+﻿using FC.Codeflix.Catalog.Infra.Data.EF;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.Common;
+public class GenrePersistence
+{
+    private readonly CodeflixCatalogDbContext _context;
+
+    public GenrePersistence(CodeflixCatalogDbContext context)
+        => _context = context;
+
+    public async Task InsertList(List<DomainEntity.Genre> genres)
+    {
+        await _context.AddRangeAsync(genres);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/GetGenre/GetGenreApiTest.cs
@@ -0,0 +1,38 @@
+﻿using FC.Codeflix.Catalog.Api.ApiModels.Response;
+using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
+using FluentAssertions;
+using System.Net;
+using Xunit;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.GetGenre;
+
+[Collection(nameof(GetGenreApiTestFixture))]
+public class GetGenreApiTest
+{
+    private readonly GetGenreApiTestFixture _fixture;
+
+    public GetGenreApiTest(GetGenreApiTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(GetGenre))]
+    [Trait("EndToEnd/API", "Genre/Get - Endpoints")]
+    public async Task GetGenre()
+    {
+        //arrange
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        var targetGenre = exampleGenres[5];
+        await _fixture.Persistence.InsertList(exampleGenres);
+
+        //act
+        var (response, output) = await _fixture.ApiClient
+            .Get<ApiResponse<GenreModelOutput>>($"/genres/{targetGenre.Id}");
+
+        //assert
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be(HttpStatusCode.OK);
+        output.Should().NotBeNull();
+        output!.Data.Id.Should().Be(targetGenre.Id);
+        output!.Data.Name.Should().Be(targetGenre.Name);
+        output!.Data.IsActive.Should().Be(targetGenre.IsActive);
+    }
+}
